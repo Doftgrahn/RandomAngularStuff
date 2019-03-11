aa1f5442121a08375907bf190fac525629bb7970
Simon grahn <simon.grahn@gmail.com>
2019-03-11 09:29:43 +0100

    kjk

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -8,18 +8,26 @@ import { NumbercalculatorService } from '../numbercalculator.service';
 })
 export class CalculatorComponent implements OnInit {
 
+  title: string = 'Excervice 4.3';
+
   calculator: number[];
+  operators: string[];
 
-
-  constructor(numberCalculatorService: NumbercalculatorService) { this.calculator = numberCalculatorService.getCourses(); }
+  constructor(numberCalculatorService: NumbercalculatorService) {
+    this.calculator = numberCalculatorService.getCourses();
+    this.operators = numberCalculatorService.getOperators();
+  }
 
   clickSimulator: number;
+
 
   clickNumbers(event: any) {
     this.clickSimulator = event.target.innerHTML;
   }
 
+
   ngOnInit() {
+
   }
 
 }
--- src/app/numbercalculator.service.ts
+++ src/app/numbercalculator.service.ts
@@ -8,6 +8,19 @@ export class NumbercalculatorService {
   getCourses() {
     return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   }
+  getOperators() {
+    return ['-', '+', '/', '*', 'Clear'];
+  }
+
+  
+
+
+
+
+
+
+
 
   constructor() { }
+
 }
